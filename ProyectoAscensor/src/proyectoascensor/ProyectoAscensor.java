0d036f3accfa81fe94e9eeb20c1a596fb28bf3be
Sergio Garcia <sergio.garcia@correounivalle.edu.co>
2014-12-06 22:29:11 -0500

    otros cambios

--- ProyectoAscensor/src/proyectoascensor/ProyectoAscensor.java
+++ ProyectoAscensor/src/proyectoascensor/ProyectoAscensor.java
@@ -13,51 +13,91 @@ import java.util.ArrayList;
  */
 public class ProyectoAscensor {
 
+    // inicio
+    double reloj = 0.0;
+
+    // variables de estado
+    int capacidadOcupada = 0;
+    String estadoAsc = "Parado";
+    String dirAsc = "arriba";
+
+    String dirDest = null;
+    int pisoDest = 1;
+    // variables de desempeño
+
+    double[] tiempoPromEsperaxpiso = {0, 0, 0, 0, 0, 0};
+    double tiempoPromEspera = 0.0;
+    double[] tiempoPromEsperaColaEntradaSubidaxpiso = {0, 0, 0, 0, 0}; // piso 1 al 5
+    double[] tiempoPromEsperaColaEntradaBajadaxpiso = {0, 0, 0, 0, 0}; // piso 2 al 6
+    double tiempoPromEsperaColaEntradaTotal = 0;
+    double[] tiempoPromEsperaColaSalidaxpiso = {0, 0, 0, 0, 0, 0}; // piso 1 al 6
+    double tiempopromEsperaColaSalidaTotal = 0;
+    double porcPersonasAtendidas = 0;
+    int capacidadOcupadaProm = 0;
+
+    // Definición del escenario 0
+    int capacidadAsc = 4;
+    int tiempoArranque = 20;
+    int despEntrePisos = 100;
     int pisoAsc = 1;
-    
-    public ArrayList crearLEF(){
+
+    public ArrayList crearLEF() {
         return null;
     }
 
-    public int[] generarPersonas(){
-        
+    public void inicializarAscensor() {
+
+    }
+
+    public int generarTiempoLlegada() {
+
         double a = 0;
-        int[] arregloPersonas;
-        int aleatorio = 0;
-        int[] arregloPisos = {1, 1, 1, 1, 2, 3, 4, 5, 6};
+        int tiempoEntreLlegadas;
         int numeroPersonas = 0;
         double lambdaPrimerPiso = 0.0025;
         double lambdaPisoUnoASeis = 0.002;
         // Math.random() retorna por defecto un valor entre 1 y 0 con una distribucion 
         // uniforme.
         double r = Math.random();
-        
-        if (pisoAsc==1) {
-            a = (-1)*(1/lambdaPrimerPiso)*Math.log(r);
-            numeroPersonas = (int) a*6 + 1;
-            arregloPersonas = new int[numeroPersonas];
-        }else{
-            a = (-1)*(1/lambdaPisoUnoASeis)*Math.log(r);
-            numeroPersonas = (int) a*6 + 1;
-            arregloPersonas = new int[numeroPersonas];
+
+        if (pisoAsc == 1) {
+            a = -(1 / lambdaPrimerPiso) * Math.log(r);
+            tiempoEntreLlegadas = (int) a;
+        } else {
+            a = -(1 / lambdaPisoUnoASeis) * Math.log(r);
+            tiempoEntreLlegadas = (int) a;
         }
-        
-        for (int i = 0; i < arregloPersonas.length; i++) {
-            aleatorio = (int) Math.floor(Math.random()*9);
-            arregloPersonas[i] = arregloPisos[aleatorio];
-        }
-        
-        return arregloPersonas;
+
+        return tiempoEntreLlegadas;
     }
-    
+
+    public int generarPiso(int pisoActual) {
+
+        int aleatorio = 0;
+        int piso = 1;
+
+        int[] arregloPisos = {1, 1, 1, 1, 2, 3, 4, 5, 6};
+        aleatorio = (int) Math.floor(Math.random() * 9);
+        piso = arregloPisos[aleatorio];
+
+        while (piso == pisoActual) {
+            aleatorio = (int) Math.floor(Math.random() * 9);
+            piso = arregloPisos[aleatorio];
+        }
+
+        return piso;
+
+    }
+
     public static void main(String[] args) {
         // TODO code application logic here
         ProyectoAscensor p = new ProyectoAscensor();
-        int[] arreglo = p.generarPersonas();
-        
-        for (int i = 0; i < arreglo.length; i++) {
-            System.out.println("Posiscion " + i + ": " + arreglo[i]);
-        }
+        int tiempo = p.generarTiempoLlegada();
+        int piso = p.generarPiso(1);
+
+        System.out.println("Tiempo: " + tiempo);
+        System.out.println("Piso: " + piso);
+
     }
-    
+
 }
